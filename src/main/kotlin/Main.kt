fdf06e6158974afb7a04feedd3d20302979fc969
hendrick <sebastinesoacatp@gmail.com>
2022-02-03 23:34:50 +0100

    oop with kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
 fun main() {
-   val day = 4
+  val myObj = MyChildClass()
+  myObj.myFunction()
+}
 
-    val result = when (day) {
-        1 -> "Monday"
-        2 -> "Tuesday"
-        3 -> "Wednesday"
-        4 -> "Thursday"
-        5 -> "Friday"
-        6 -> "Saturday"
-        7 -> "Sunday"
-        else -> "Invalid day."
-    }
+fun sumOfTwoNumbers(first: Int, second: Int) : Int {
+  return first + second
+}
 
-    println(result)
+
+open class MyParentClass {
+  val x = 5
+}
+
+class MyChildClass: MyParentClass() {
+  fun myFunction() {
+    println(x)
+  }
 }
